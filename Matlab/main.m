0a79a571f3ea0fef21e9788383d5d61acffc5ea2
Knoxantropicen <knoxtian@icloud.com>
2017-12-08 12:03:43 +0800

    fix bugs

--- Matlab/main.m
+++ Matlab/main.m
@@ -1,13 +1,15 @@
 clear ; close all; clc
 
 %% Global parameters
-IMAGE_NAME = 'brain16';
+IMAGE_NAME = 'brain8';
 MAX_ROUND = 2;
 SCHEME = 'MA';
 
 %% Initial processing
 K_LEN = 104+MAX_ROUND*76;
 assert(strcmp(SCHEME,'BX')||strcmp(SCHEME,'MA'));
+
+t_in = Tiff(strcat('input_image/',IMAGE_NAME,'.tif'));
 
 %% Main program
 
@@ -19,29 +21,36 @@ K = round(rand(1,K_LEN));
 C = encryption(P,K,SCHEME,MAX_ROUND);
 
 % Result output
-t = Tiff(strcat('output_image/',IMAGE_NAME,'_encrypted.tif'),'w');
-tagstruct.ImageLength     = size(C,1);
-tagstruct.ImageWidth      = size(C,2);
-tagstruct.Photometric     = Tiff.Photometric.MinIsBlack;
-tagstruct.BitsPerSample   = 8;
-tagstruct.SamplesPerPixel = size(C,3);
-tagstruct.PlanarConfiguration = Tiff.PlanarConfiguration.Chunky;
-t.setTag(tagstruct);
-t.write(uint8(C));
-t.close();
+t_out = Tiff(strcat('output_image/',IMAGE_NAME,'_encrypted.tif'),'w');
+tagstruct.ImageLength     = t_in.getTag('ImageLength')+2;
+tagstruct.ImageWidth      = t_in.getTag('ImageWidth')+2;
+tagstruct.Photometric     = t_in.getTag('Photometric');
+tagstruct.BitsPerSample   = t_in.getTag('BitsPerSample');
+tagstruct.SamplesPerPixel = t_in.getTag('SamplesPerPixel');
+tagstruct.PlanarConfiguration = t_in.getTag('PlanarConfiguration');
+if tagstruct.Photometric == Tiff.Photometric.RGB && tagstruct.SamplesPerPixel == 4
+    tagstruct.ExtraSamples = t_in.getTag('ExtraSamples');
+end
+t_out.setTag(tagstruct);
+t_out.write(uint8(C));
+t_out.close();
 
 % Decryption
 D = decryption(C,K,SCHEME,MAX_ROUND);
 
 % Result output
-t = Tiff(strcat('output_image/',IMAGE_NAME,'_decrypted.tif'),'w');
-tagstruct.ImageLength     = size(D,1);
-tagstruct.ImageWidth      = size(D,2);
-tagstruct.Photometric     = Tiff.Photometric.MinIsBlack;
-tagstruct.BitsPerSample   = 8;
-tagstruct.SamplesPerPixel = size(D,3);
-tagstruct.PlanarConfiguration = Tiff.PlanarConfiguration.Chunky;
-t.setTag(tagstruct);
-t.write(uint8(D));
-t.close();
+t_out = Tiff(strcat('output_image/',IMAGE_NAME,'_decrypted.tif'),'w');
+tagstruct.ImageLength     = t_in.getTag('ImageLength');
+tagstruct.ImageWidth      = t_in.getTag('ImageWidth');
+tagstruct.Photometric     = t_in.getTag('Photometric');
+tagstruct.BitsPerSample   = t_in.getTag('BitsPerSample');
+tagstruct.SamplesPerPixel = t_in.getTag('SamplesPerPixel');
+tagstruct.PlanarConfiguration = t_in.getTag('PlanarConfiguration');
+if tagstruct.Photometric == Tiff.Photometric.RGB && tagstruct.SamplesPerPixel == 4
+    tagstruct.ExtraSamples = t_in.getTag('ExtraSamples');
+end
+t_out.setTag(tagstruct);
+t_out.write(uint8(D));
+t_out.close();
 
+t_in.close();
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ISProject
-A Python/Matlab implementation of "Medical image encryption using high-speed scrambling and pixel adaptive diffusion". Not yet completed due to some unfixed bugs.
+A Python/Matlab implementation of "Medical image encryption using high-speed scrambling and pixel adaptive diffusion".
