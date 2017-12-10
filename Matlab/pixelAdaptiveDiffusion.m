ffbdc6782ee76025e64b45ad8c553fe8315e442d
Knoxantropicen <knoxtian@icloud.com>
2017-12-11 01:25:11 +0800

    fix bugs

--- Matlab/pixelAdaptiveDiffusion.m
+++ Matlab/pixelAdaptiveDiffusion.m
@@ -10,47 +10,55 @@ C = zeros(M,N,CN);
 
 if strcmp(STATE, 'EN')
     if strcmp(SCHEME, 'BX')
-        C(1,1,:) = bitxor(bitxor(T(1,1,:),T(M,N,:)),Q(1,1,:));
-        for j = 2:N
-            C(1,j,:) = bitxor(bitxor(T(1,j,:),C(M,j-1,:)),Q(1,j,:));
-        end
-        for i = 2:M
-            for j = 1:N
-                C(i,j,:) = bitxor(bitxor(T(i,j,:),C(i-1,j,:)),Q(i,j,:));
+        for j = 1:N
+            for i = 1:M
+                if i == 1 && j == 1
+                    C(i,j,:) = bitxor(bitxor(T(i,j,:),T(M,N,:)),Q(i,j,:));
+                elseif i == 1
+                    C(i,j,:) = bitxor(bitxor(T(i,j,:),C(M,j-1,:)),Q(i,j,:));
+                else
+                    C(i,j,:) = bitxor(bitxor(T(i,j,:),C(i-1,j,:)),Q(i,j,:));
+                end
             end
         end
     else
-        C(1,1,:) = mod(T(1,1,:)+T(M,N,:)+Q(1,1,:),F);
-        for j = 2:N
-            C(1,j,:) = mod(T(1,j,:)+C(M,j-1,:)+Q(1,j,:),F);
-        end
-        for i = 2:M
-            for j = 1:N
-                C(i,j,:) = mod(T(i,j,:)+C(i-1,j,:)+Q(i,j,:),F);
+        for j = 1:N
+            for i = 1:M
+                if i == 1 && j == 1
+                    C(i,j,:) = mod(T(i,j,:)+T(M,N,:)+Q(i,j,:),F);
+                elseif i == 1
+                    C(i,j,:) = mod(T(i,j,:)+C(M,j-1,:)+Q(i,j,:),F);
+                else
+                    C(i,j,:) = mod(T(i,j,:)+C(i-1,j,:)+Q(i,j,:),F);
+                end
             end
         end
     end
 else
     if strcmp(SCHEME, 'BX')
-        for i = M:-1:2
-            for j = N:-1:1
-                C(i,j,:) = bitxor(bitxor(T(i,j,:),T(i-1,j,:)),Q(i,j,:));
+        for j = N:-1:1
+            for i = M:-1:1
+                if i == 1 && j == 1
+                    C(i,j,:) = bitxor(bitxor(T(i,j,:),C(M,N,:)),Q(i,j,:));
+                elseif i == 1
+                    C(i,j,:) = bitxor(bitxor(T(i,j,:),T(M,j-1,:)),Q(i,j,:));
+                else
+                    C(i,j,:) = bitxor(bitxor(T(i,j,:),T(i-1,j,:)),Q(i,j,:));
+                end
             end
         end
-        for j = N:-1:2
-            C(1,j,:) = bitxor(bitxor(T(1,j,:),T(M,j-1,:)),Q(1,j,:));
-        end
-        C(1,1,:) = bitxor(bitxor(T(1,1,:),C(M,N,:)),Q(1,1,:));
     else
-        for i = M:-1:2
-            for j = N:-1:1
-                C(i,j,:) = mod(T(i,j,:)-T(i-1,j,:)-Q(i,j,:),F);
+        for j = N:-1:1
+            for i = M:-1:1
+                if i == 1 && j == 1
+                    C(i,j,:) = mod(T(i,j,:)-C(M,N,:)-Q(i,j,:),F);
+                elseif i == 1
+                    C(i,j,:) = mod(T(i,j,:)-T(M,j-1,:)-Q(i,j,:),F);
+                else
+                    C(i,j,:) = mod(T(i,j,:)-T(i-1,j,:)-Q(i,j,:),F);
+                end
             end
         end
-        for j = N:-1:2
-            C(1,j,:) = mod(T(1,j,:)-T(M,j-1,:)-Q(1,j,:),F);
-        end
-        C(1,1,:) = mod(T(1,1,:)-C(M,N,:)-Q(1,1,:),F);
     end
 end
 
